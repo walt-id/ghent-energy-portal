b72df1b7b6948a0c0cd35d17dc1d0079c7480c81
Severin Stampler <severin@walt.id>
2023-04-12 16:02:39 +0200

    revert enrollment page back to previous version

--- pages/Enrollment.vue
+++ pages/Enrollment.vue
@@ -19,7 +19,7 @@
           <div>
             <h2 class="text-lg font-semibold mb-2">Install your wallet</h2>
             <p class="mb-2">
-              Install a SSI wallet or use the EUDI web wallet. Wallets are available for both Android in the Google Play
+              Install a SSI wallet or use the NEOM web wallet. Wallets are available for both Android in the Google Play
               Store, and iOS in the Apple AppStore. </p>
             <div class="flex space-x-2 ml-auto justify-end">
               <a href="https://play.google.com/store/apps/details?id=com.validatedid.wallet" target="_blank"
@@ -46,8 +46,20 @@
           <div class="text-4xl font-bold text-gray-500">2</div>
           <div>
             <h2 class="text-lg font-semibold mb-2">
-              Request your StudentId and Diploma credential from your home university</h2>
-              <p>Go to the portal of your EU university to obtain the StudentId and Diploma credentials into your EUDI wallet</p>
+              Request your VerifiableID at NEOM Issuer </h2>
+            <p class="mb-4">
+              Neom Issuer can issue you your VerifiableID. </p>
+            <a class="
+                my-2
+                rounded
+                px-4
+                py-2
+                hover:text-gray-700
+                bg-gray-300
+                text-gray-600
+                rounded
+              " href="https://onboarding.neom.walt-test.cloud/NeomSignup" target="_blank"
+            >Request</a>
           </div>
         </li>
         <li class="
@@ -65,7 +77,104 @@
         >
           <div class="text-4xl font-bold text-gray-500">3</div>
           <div>
-            <h2 class="text-lg font-semibold mb-2">Apply for EU Student Enrollment</h2>
+            <h2 class="text-lg font-semibold mb-2">
+              Request your Verifiable Credentials at the Federal Portal </h2>
+            <p class="mb-4">
+              NEOM lets you obtain the Verifiable Credentials you need to apply for student enrollment from federal
+              government services. You can authenticate with your VerifiableID. </p>
+            <a class="
+                my-2
+                rounded
+                px-4
+                py-2
+                hover:text-gray-700
+                bg-gray-300
+                text-gray-600
+                rounded
+              " href="https://onboarding.neom.walt-test.cloud/NeomPortal" target="_blank"
+            >Request</a>
+          </div>
+        </li>
+        <li class="
+            flex
+            space-x-4
+            my-2
+            shadow
+            px-4
+            py-4
+            hover:bg-gray-100 hover:text-gray-700
+            bg-white
+            text-gray-600
+            rounded
+          "
+        >
+          <div class="text-4xl font-bold text-gray-500">4</div>
+          <div>
+            <h2 class="text-lg font-semibold mb-2">
+              Request your ABC at XYZ </h2>
+            <p class="mb-4">
+              ... </p>
+            <a class="
+                my-2
+                rounded
+                px-4
+                py-2
+                hover:text-gray-700
+                bg-gray-300
+                text-gray-600
+                rounded
+              " href="#" target="_blank"
+            >...</a>
+          </div>
+        </li>
+        <li class="
+            flex
+            space-x-4
+            my-2
+            shadow
+            px-4
+            py-4
+            hover:bg-gray-100 hover:text-gray-700
+            bg-white
+            text-gray-600
+            rounded
+          "
+        >
+          <div class="text-4xl font-bold text-gray-500">5</div>
+          <div>
+            <h2 class="text-lg font-semibold mb-2">
+              Request your DEF at UVW </h2>
+            <p class="mb-4">
+              ... </p>
+            <a class="
+                my-2
+                rounded
+                px-4
+                py-2
+                hover:text-gray-700
+                bg-gray-300
+                text-gray-600
+                rounded
+              " href="#" target="_blank"
+            >...</a>
+          </div>
+        </li>
+        <li class="
+            flex
+            space-x-4
+            my-2
+            shadow
+            px-4
+            py-4
+            hover:bg-gray-100 hover:text-gray-700
+            bg-white
+            text-gray-600
+            rounded
+          "
+        >
+          <div class="text-4xl font-bold text-gray-500">6</div>
+          <div>
+            <h2 class="text-lg font-semibold mb-2">Apply for Student Enrollment</h2>
             <p class="mb-4">
               Present the required Verifiable Credentials to us to obtain your student enrollment approval.</p>
             <nuxt-link class="
